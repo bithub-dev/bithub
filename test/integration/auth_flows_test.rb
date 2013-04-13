257aa887c60acc8bfc2554d68c876dded45bd1b3
Jesse Buchanan <jbinto@jbinto.ca>
2013-04-13 15:21:16 -0400

    Show login form if login fails. Fix #10.
    * Also added integration test.

--- test/integration/auth_flows_test.rb
+++ test/integration/auth_flows_test.rb
@@ -1,4 +1,10 @@
 require 'test_helper'
 
 class AuthFlowsTest < ActionDispatch::IntegrationTest
+
+  test "invalid login shows errors" do
+    visit login_path
+    click_button 'Login'
+    assert find('.alert-error').has_content?('try again')
+  end
 end
