8b9a656f501f7b8dc4b6efcf691f1f25c270bfe4
Shane Fera <shane.d.fera@gmail.com>
2013-04-13 12:45:08 -0400

    Almost completed login

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,12 @@ Bithub::Application.routes.draw do
   resources :tips
   resources :resources
   resources :students, only: ['index']
-  resources :users, except: ['index']
+  get "logout" => "sessions#destroy", :as => "logout"
+  get "login" => "sessions#new", :as => "login"
+  get "signup" => "users#new", :as => "signup"
+  resources :users
+  resources :sessions
+  get "secret" => "home#secret", :as => "secret"
+  root :to => "home#index"
+  get "sessions/create" => "home#index"
 end
