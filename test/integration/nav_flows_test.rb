c41b5edfc2adadf2f5911c311df20758e96379d2
Jesse Buchanan <jbinto@jbinto.ca>
2013-04-13 11:07:14 -0400

    Add integration tests for navbar.
    * Add Capybara to ActionDispatch::IntegrationTest
    * Set up Guardfile (now just run `bundle exec guard`)
    * Remove all fixtures
    * Remove performance tests to make Guard happy

---
+++ test/integration/nav_flows_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class NavFlowsTest < ActionDispatch::IntegrationTest
+  test 'home has nothing selected' do
+    visit '/'
+    assert page.has_no_selector?('.navbar li.active')
+  end
+
+  test 'students has students selected' do
+    assert clicking_on_nav_activates('Students')
+  end
+
+  test 'tips has tips selected' do
+    assert clicking_on_nav_activates('Tips & Tricks')
+  end
+
+  test 'resouces has resources selected' do
+    assert clicking_on_nav_activates('Resources')
+  end
+
+  def clicking_on_nav_activates(link_text)
+    visit '/'
+    click_link link_text
+    find('.navbar li.active').has_content?(link_text)
+  end
+
+
+end
