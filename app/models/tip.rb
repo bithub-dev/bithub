1490360f020494e774058734706c03deabb9872d
Jesse Buchanan <jbinto@jbinto.ca>
2013-04-14 14:04:22 -0400

    Tip acts_as_taggable. Improve tag clouds on home page.

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -1,6 +1,8 @@
 class Tip < ActiveRecord::Base
   belongs_to :user
-  attr_accessible :title, :url, :address, :description
+  attr_accessible :title, :url, :address, :description, :tag_list
+
+  acts_as_taggable
 
   validates :title, :presence => true
   validates :user, :presence => true
