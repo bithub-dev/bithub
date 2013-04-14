a410b400445c1070901fe1c0d30d133d88b3a50c
Jesse Buchanan <jbinto@jbinto.ca>
2013-04-13 20:32:59 -0400

    Use Bootstrap provided Glyphicons on buttons. Fix JS issue when validation fails on add resource.

--- app/assets/javascripts/resources/index.js
+++ app/assets/javascripts/resources/index.js
@@ -1,14 +1,25 @@
 $(function() {
   $form = $('.new_product_form');
   $btnNew = $('.btn-add-resource');
+  $formAlert = $('form .alert');
 
+  /* If there are any errors on the form, hide the add button  */
+  if ($formAlert.length)
+  {
+    $btnNew.hide(); 
+  }
+  else {
+    /* Hide the form and set it's opacity to 0 for the transition */
+    $form.hide();
+    $form.css({opacity: 0});
+  }
 
-  $form.hide();
-  $form.css({opacity: 0});
-
+  /* Fade the form in when user clicks add. Hide the button. */
   $btnNew.click(function() {
     $form.show();
     $form.transition({ opacity: 1 });
     $(this).transition({ opacity: 0 });
   });
+
 });
+
